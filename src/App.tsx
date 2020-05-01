d9e489c517c70fd28acbd2d24f30dd9065b9826f
unknown <a.c.rezai@gmail.com>
2020-05-01 23:05:11 +0200

    -ADD react-router, IonReactRouter and test tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { Redirect, Route } from 'react-router-dom';
 import {
   IonApp,
   IonTabs,
+  IonRouterOutlet,
   IonTabBar,
   IonTabButton,
   IonIcon,
   IonLabel,
   IonBadge,
 } from "@ionic/react";
+import { IonReactRouter } from '@ionic/react-router';
 import { calendar, personCircle, map, informationCircle } from "ionicons/icons";
+
+import Tab1 from './pages/Tab1';
+import Tab2 from './pages/Tab2';
+import Tab3 from './pages/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
@@ -33,7 +40,16 @@ const App: React.FC = () => {
   return (
     <React.Fragment>
       <IonApp>
+      <IonReactRouter>
+
         <IonTabs>
+        <IonRouterOutlet>
+          <Route path="/tab1" component={Tab1} exact={true} />
+          <Route path="/tab2" component={Tab2} exact={true} />
+          <Route path="/tab3" component={Tab3} />
+          <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
+        </IonRouterOutlet>
+
           <IonTabBar slot="bottom">
             <IonTabButton tab="schedule">
               <IonIcon icon={calendar} />
@@ -57,6 +73,8 @@ const App: React.FC = () => {
             </IonTabButton>
           </IonTabBar>
         </IonTabs>
+        </IonReactRouter>
+
       </IonApp>
     </React.Fragment>
   );
