2ce3229dc78e8bd2684abeee4c7d5701562a1e03
unknown <a.c.rezai@gmail.com>
2020-05-02 13:57:44 +0200

    -ADD (WiP) menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
-import { Redirect, Route } from 'react-router-dom';
+import { Redirect, Route } from "react-router-dom";
 import {
   IonApp,
+  IonMenu,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
   IonTabs,
   IonRouterOutlet,
   IonTabBar,
@@ -10,13 +17,13 @@ import {
   IonLabel,
   IonBadge,
 } from "@ionic/react";
-import { IonReactRouter } from '@ionic/react-router';
+import { IonReactRouter } from "@ionic/react-router";
 import { calendar, personCircle, map, informationCircle } from "ionicons/icons";
 
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
-import Tab4 from './pages/Tab4';
+import Tab1 from "./pages/Tab1";
+import Tab2 from "./pages/Tab2";
+import Tab3 from "./pages/Tab3";
+import Tab4 from "./pages/Tab4";
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
@@ -41,42 +48,60 @@ const App: React.FC = () => {
   return (
     <React.Fragment>
       <IonApp>
-      <IonReactRouter>
+        <IonMenu type="overlay" contentId="main">
+          <IonHeader>
+            <IonToolbar color="primary">
+              <IonTitle>Start Menu</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent id="main">
+            <IonList>
+              <IonItem>Menu Item</IonItem>
+              <IonItem>Menu Item</IonItem>
+              <IonItem>Menu Item</IonItem>
+              <IonItem>Menu Item</IonItem>
+              <IonItem>Menu Item</IonItem>
+            </IonList>
+          </IonContent>
+        </IonMenu>
+        <IonReactRouter>
+          <IonTabs>
+            <IonRouterOutlet>
+              <Route path="/tab1" component={Tab1} exact={true} />
+              <Route path="/tab2" component={Tab2} exact={true} />
+              <Route path="/tab3" component={Tab3} exact={true} />
+              <Route path="/tab4" component={Tab4} />
+              <Route
+                path="/"
+                render={() => <Redirect to="/tab1" />}
+                exact={true}
+              />
+            </IonRouterOutlet>
 
-        <IonTabs>
-        <IonRouterOutlet>
-          <Route path="/tab1" component={Tab1} exact={true} />
-          <Route path="/tab2" component={Tab2} exact={true} />
-          <Route path="/tab3" component={Tab3} exact={true} />
-          <Route path="/tab4" component={Tab4} />
-          <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
-        </IonRouterOutlet>
+            <IonTabBar slot="bottom">
+              <IonTabButton tab="schedule" href="/tab1">
+                <IonIcon icon={calendar} />
+                <IonLabel>Schedule</IonLabel>
+                <IonBadge>6</IonBadge>
+              </IonTabButton>
 
-          <IonTabBar slot="bottom">
-            <IonTabButton tab="schedule" href='/tab1'>
-              <IonIcon icon={calendar} />
-              <IonLabel>Schedule</IonLabel>
-              <IonBadge>6</IonBadge>
-            </IonTabButton>
+              <IonTabButton tab="speakers" href="/tab2">
+                <IonIcon icon={personCircle} />
+                <IonLabel>Speakers</IonLabel>
+              </IonTabButton>
 
-            <IonTabButton tab="speakers" href='/tab2'>
-              <IonIcon icon={personCircle} />
-              <IonLabel>Speakers</IonLabel>
-            </IonTabButton>
+              <IonTabButton tab="map" href="/tab3">
+                <IonIcon icon={map} />
+                <IonLabel>Map</IonLabel>
+              </IonTabButton>
 
-            <IonTabButton tab="map" href='/tab3'>
-              <IonIcon icon={map} />
-              <IonLabel>Map</IonLabel>
-            </IonTabButton>
-
-            <IonTabButton tab="about"  href='/tab4'>
-              <IonIcon icon={informationCircle} />
-              <IonLabel>About</IonLabel>
-            </IonTabButton>
-          </IonTabBar>
-        </IonTabs>
+              <IonTabButton tab="about" href="/tab4">
+                <IonIcon icon={informationCircle} />
+                <IonLabel>About</IonLabel>
+              </IonTabButton>
+            </IonTabBar>
+          </IonTabs>
         </IonReactRouter>
-
       </IonApp>
     </React.Fragment>
   );
